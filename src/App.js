d3349b07262e68d5c02abe77fc63009b0d9ee99c
gaurii <gauribirari2708@gmail.com>
2024-09-11 11:21:19 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
 
-function App() {
+const App = () => {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  // get data function
+  const getData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users", {
+      headers: {
+        "Content-type": "application/json"
+      }
+    })
+      .then((response) => {
+        console.log(response.data)
+        setData(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Get User Data Using Axios</h1>
+      <table className='table table-striped table-bordered'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>User Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((item) => {
+              return (
+                <>
+                  <tr>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.username}</td>
+                  </tr>
+                </>
+              )
+            })
+          }
+        </tbody>
+      </table>
+
+    </>
+  )
 }
 
-export default App;
+export default App
